94f1c49bd2611c82ae604dc59d1f0ce12e40f0f5
luckhee <dcs1206@naver.com>
2025-06-01 23:30:31 +0900

    11단계

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Calc {
     static int result = 0;
+    static int cnt =0;
     static int run(String calc) {
         calc = calc.replaceAll(" ","");
         if(calc.contains("+")) {
@@ -12,7 +13,7 @@ public class Calc {
             for(String x : splitCf) {
                 if(x.contains("-")) {
                     String[] minusSplitCf = x.split("-");
-                    System.out.println("minus  = "+Arrays.toString(minusSplitCf));
+                    System.out.println(cnt+"번 minus  = "+Arrays.toString(minusSplitCf));
                     result += Integer.parseInt(minusSplitCf[0]);
                     System.out.println("1번 " +result);
                     minusSplitCf[0]="0";
@@ -20,16 +21,24 @@ public class Calc {
                         result -= Integer.parseInt(y);
                         System.out.println("2번 " +result);
                     }
-                    splitCf[0]="0";
+                    System.out.println("result = " + result);
+                    splitCf[cnt++]="0";
+                    System.out.println(Arrays.toString(splitCf));
+
+
+                } else {
+                    result += Integer.parseInt(x);
+                    //cnt++;
                 }
             }
             for(String x : splitCf) {
-                result += Integer.parseInt(x);
-                System.out.println("3번 " +result);
+                return result += Integer.parseInt(x);
+                //System.out.println("3번 " +result);
             }
             System.out.println("최종 result = " +result);
             return result;
         }
+
         if(calc.contains("-")) {
 
             String[] splitCf = calc.split("-");
