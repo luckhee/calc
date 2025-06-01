e719b685f4b78e9d813d53e76176cacecbe1568f
luckhee <dcs1206@naver.com>
2025-06-02 00:20:51 +0900

    14단계

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,67 +1,49 @@
 package com.back;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Calc {
     static int result;
-    static int cnt =0;
+
     static int run(String calc) {
-        calc = calc.replaceAll(" ","");
-        if(calc.contains("+")) {
-            String[] splitCf = calc.split("\\+"); // [10-10-10-10, 10, 10-10]
-            System.out.println(Arrays.toString(splitCf));
-            for(String x : splitCf) {
-                if(x.contains("-")) {
-                    String[] minusSplitCf = x.split("-");
-                    System.out.println(cnt+"번 minus  = "+Arrays.toString(minusSplitCf));
-                    result += Integer.parseInt(minusSplitCf[0]);
-                    System.out.println("1번 " +result);
-                    minusSplitCf[0]="0";
-                    for(String y : minusSplitCf) {
-                        result -= Integer.parseInt(y);
-                        System.out.println("2번 " +result);
+        result = 0;
+        calc = calc.replaceAll(" ", "");
+
+        if (calc.contains("+")) {
+            String[] plusParts = calc.split("\\+");
+            for (int i = 0; i < plusParts.length; i++) {
+                String part = plusParts[i];
+                if (part.contains("-")) {
+                    String[] minusParts = part.split("-");
+                    result += Integer.parseInt(minusParts[0]);
+                    for (int j = 1; j < minusParts.length; j++) {
+                        result -= Integer.parseInt(minusParts[j]);
                     }
-                    System.out.println("result = " + result);
-                    splitCf[cnt++]="0";
-                    System.out.println(Arrays.toString(splitCf));
-
-
-                }
-                else {
-                    result += Integer.parseInt(x);
-                    //cnt++;
+                } else {
+                    result += Integer.parseInt(part);
                 }
             }
-//            for(String x : splitCf) {
-//                result += Integer.parseInt(x);
-//                System.out.println("3번 " +result);
-//            }
-            System.out.println("최종 result = " +result);
             return result;
-        } else if(calc.contains("-")) {
-
-            String[] splitCf = calc.split("-");
-            //System.out.println(Arrays.toString(splitCf));
-            result += Integer.parseInt(splitCf[0]);
-            splitCf[0]="0";
-            for(String x : splitCf) {
-                result -= Integer.parseInt(x);
-            }
-        } else if(calc.contains("*")) {
-            List<String> list = new ArrayList<>(Arrays.asList(calc.split("\\*")));
-            System.out.println(list);
-            int answer =1;
-            for( String x: list) {
-                answer *= Integer.parseInt(x);
-                result = answer;
-            }
-
-
-
         }
+
+        if (calc.contains("*")) {
+            String[] multiplyParts = calc.split("\\*");
+            result = 1;
+            for (String part : multiplyParts) {
+                result *= Integer.parseInt(part);
+            }
+            return result;
+        }
+
+        if (calc.contains("-")) {
+            String[] minusParts = calc.split("-");
+            result = Integer.parseInt(minusParts[0]);
+            for (int i = 1; i < minusParts.length; i++) {
+                result -= Integer.parseInt(minusParts[i]);
+            }
+            return result;
+        }
+
+
+
         return result;
     }
-
 }
