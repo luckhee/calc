dca9a78e1f28cb4b8e94de1e5689022f19b301d4
luckhee <dcs1206@naver.com>
2025-06-12 21:32:54 +0900

    level 12 fail

--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -71,5 +71,23 @@ public class CalcTest {
                 isEqualTo(20);
     }
 
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 == -20")
+    void t10() {
+        assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+    void t11() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
+
+    @Test
+    @DisplayName("10 * 10 == 100")
+    void t12() {
+        assertThat(Calc.
+                run("10 * 10")).isEqualTo(100);
+    }
 
 }
